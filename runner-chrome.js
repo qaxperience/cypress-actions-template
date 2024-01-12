805f6ec73883adc18bee541f335b8b35e239cb47
thallysrch <52187772+thallysrch@users.noreply.github.com>
2024-01-12 11:42:55 -0300

    Update runner-chrome.js

--- runner-chrome.js
+++ runner-chrome.js
@@ -2,7 +2,7 @@ const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
 cypress.run({
-  // specs to run here
+ browser: 'chrome'
 })
 .then((results) => {
   const args = {
