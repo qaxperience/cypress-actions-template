cf9f82415d7ceb843c368dbd4b70afd9d65ba918
thallysrch <52187772+thallysrch@users.noreply.github.com>
2024-01-12 11:43:17 -0300

    Update runner-firefox.js

--- runner-firefox.js
+++ runner-firefox.js
@@ -2,7 +2,7 @@ const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
 cypress.run({
-  // specs to run here
+  browser: 'firefox'
 })
 .then((results) => {
   const args = {
